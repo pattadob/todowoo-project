3c9bf85e98cd891c88409de0d22eb138681381d2
pattadob <pattadon.b@gmail.com>
2020-05-28 22:24:33 +0700

    Creating Todos

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -37,6 +38,19 @@ def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False) # No commit to database
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Bad data passed in. Try again.'})
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
